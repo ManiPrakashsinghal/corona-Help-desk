5d27774d2beff6748310538b156555c233469890
unknown <psks1204@gmail.com>
2020-03-25 20:08:02 +0530

    registration done

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ShareService } from '../services/share.service';
 
 @Component({
@@ -17,18 +17,28 @@ export class RegistrationPage implements OnInit {
     private shareService: ShareService
   ) { 
     this.registrationForm = this.fb.group({
-      name: [null],
-      address: [null],
-      pinCode: [null],
-      phoneNo: [null],
-      description: [null],
+      name: [null, [Validators.required, Validators.minLength(3)]],
+      address: [null, [Validators.required]],
+      pinCode: [null, [Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
+      phoneNo: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      title: [null, [Validators.required]],
+      description: [null, [Validators.required]],
       // city: [null],
       // state: [null]
     })
   }
 
+  get errorControl() {
+    return this.registrationForm.controls;
+  }
+
   ngOnInit() {
     this.mode = this.shareService.getData('mode');
+  }
+
+  register(mode: String){
+    console.log("user", this.registrationForm.value);
+    localStorage.setItem('user', JSON.stringify(this.registrationForm.value))
   }
 
 }
