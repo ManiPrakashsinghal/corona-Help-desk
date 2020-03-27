d97f2fd2ee035a2866561a05b8b216cb8899b5db
deepak dhakar <deepakdhaker8233@gmail.com>
2020-03-27 18:35:10 +0530

    changes p

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/Fire';
-import { environment } from '../environments/environment'
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
